1ff781a1b03fbdb67797486ac5f553e913bffbac
Tandy Sabana <tansabana12@gmail.com>
2024-05-22 09:17:32 +0700

    Initial Commit

---
+++ Jan-KenMonster/Jan_KenMonsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jan_KenMonsterApp.swift
+//  Jan-KenMonster
+//
+//  Created by tandyys on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Jan_KenMonsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
